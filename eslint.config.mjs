0db984c83d2f911ad19a26fbca50c123a19045e8
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2026-01-08 15:09:33 -0500

    feat: Add Header and Page components with Storybook stories
    - Created Header component with user login/logout functionality.
    - Developed Page component integrating Header and showcasing various sections.
    - Added Storybook stories for both Header and Page components to demonstrate different states (LoggedIn, LoggedOut).
    - Included CSS styles for Header, Page, and Button components to enhance UI.
    - Introduced various assets (images and SVGs) for use in the Storybook environment.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 // @ts-check
 import {defineConfig} from 'eslint/config';
 import baseConfig from './config/eslint/eslint.config.base.mjs';
